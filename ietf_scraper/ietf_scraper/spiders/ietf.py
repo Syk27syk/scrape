034eea2217cb13cac84020fc45d0a36923580c05
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-14 11:00:50 +0800

    scrape page title with xpath

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -4,9 +4,9 @@ import scrapy
 class IetfSpider(scrapy.Spider):
     name = 'ietf'
     allowed_domains = ['pythonscraping.com']
-    start_urls = ['http://pythonscraping.com/']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
 
     def parse(self, response):
         # title = response.css('span.title:text').get()
         title = response.xpath('//span[@class="title"]/text()').get()
-        pass
+        return {"title": title}
