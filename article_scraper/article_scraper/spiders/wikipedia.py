f6c2850df437da90973d87476f1e3c5854646252
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-14 14:47:45 +0800

    wikipedia.py file not found

--- article_scraper/article_scraper/spiders/wikipedia.py
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -1,10 +1,19 @@
 import scrapy
-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 class WikipediaSpider(scrapy.Spider):
     name = 'wikipedia'
     allowed_domains = ['en.wikipedia.org']
-    start_urls = ['http://en.wikipedia.org/']
+    start_urls = ['http://en.wikipedia.org/wiki/Kevin_Bacon']
+
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info',
+    follow=True)]
 
     def parse(self, response):
-        pass
+        return {
+            'title': response.xpath('//h1/text()').get() or response.xpath('//h1/1/text()').get(),
+            'url': response.url,
+            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        }
+
